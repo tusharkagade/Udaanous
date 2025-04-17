e7ec0a26f2a02dd00273c17f3d37294deb1846bb
tusharkagade <145856946+tusharkagade@users.noreply.github.com>
2025-04-17 05:24:17 -0700

    Add files via upload

---
+++ mysql/year_function_task.sql
@@ -0,0 +1,18 @@
+CREATE TABLE purchases (
+    CustomerID INT,
+    PurchaseDate DATE,
+    ReturnDate DATE
+);
+INSERT INTO purchases (CustomerID, PurchaseDate, ReturnDate) VALUES
+(1, '2024-01-01', '2024-01-05'),
+(2, '2024-01-02', '2024-01-06'),
+(3, '2024-01-03', '2024-01-07'),
+(4, '2024-01-04', '2024-01-08'),
+(5, '2024-01-05', '2024-01-09'),
+(6, '2024-01-06', '2024-01-10'),
+(7, '2024-01-07', '2024-01-11'),
+(8, '2024-01-08', '2024-01-12'),
+(9, '2024-01-09', '2024-01-13'),
+(10, '2024-01-10', '2024-01-14');
+
+select year(ReturnDate) as YearsData from purchases; 
